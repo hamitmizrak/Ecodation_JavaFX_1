905f8f8082d17bab2fec3b5c6e48e57784d0f351
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-03 19:08:07 +0300

    DBConnection

---
+++ src/main/java/com/hamitmizrak/ecodation_javafx/database/DBConnection.java
@@ -0,0 +1,20 @@
+package com.hamitmizrak.ecodation_javafx.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/user_management";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "rootroot";
+
+    // import java.sql.Connection;
+    public static Connection getConnection() {
+        try {
+            return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (Exception e) {
+            throw new RuntimeException("Veri tabanınan bağlanılmadı");
+        }
+    } //end getConnection
+}
