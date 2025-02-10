44a1fa9c9639d25ee2bb26bd616a06150581547b
Hamit Mizrak <hamitmizrak@gmail.com>
2025-02-10 19:21:29 +0300

    login.fxml

--- src/main/java/com/hamitmizrak/ecodation_javafx/HelloApplication.java
+++ src/main/java/com/hamitmizrak/ecodation_javafx/HelloApplication.java
@@ -2,6 +2,7 @@ package com.hamitmizrak.ecodation_javafx;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,14 +11,21 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
+        /*
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
+        stage.show();*/
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("view/login.fxml"));
+        Parent root = fxmlLoader.load();
+        stage.setTitle("Kullanıcı Kayıt!");
+        stage.setScene(new Scene(root));
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
-    }
-}
+        launch(args);
+    } //end psvm
+} //end HelloApplication
--- src/main/java/com/hamitmizrak/ecodation_javafx/controller/RegisterController.java
+++ src/main/java/com/hamitmizrak/ecodation_javafx/controller/RegisterController.java
@@ -42,6 +42,7 @@ public class RegisterController {
 
     /////////////////////////////////////////////////////////////////////////////////////////
     // Switch
+    @FXML
     private void switchToLogin() {
         try {
             // FXML Dosyalarını Yükle
